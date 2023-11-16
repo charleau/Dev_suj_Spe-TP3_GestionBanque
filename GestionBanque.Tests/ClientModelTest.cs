a413b5129bdfd58183575f4882e015398d8566ae
Charles Ricard <djcharleau.ricard@gmail.com>
2023-11-15 23:38:16 -0500

    Ajoutez des fichiers projet.

---
+++ GestionBanque.Tests/ClientModelTest.cs
@@ -0,0 +1,50 @@
+﻿using GestionBanque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionBanque.Tests
+{
+    public class ClientModelTest
+    {
+
+        [Fact]
+        public void TestSetterNom_ShouldBeEqual()
+        {
+            Client client = new Client(1, "Ndong", "Saliou", "saliou@ndong.com");
+
+            client.Nom = "Test";
+
+            Assert.Equal("Test", client.Nom);
+        }
+
+        [Fact]
+        public void TestSetterPrenom_ShouldThrowException()
+        {
+            Client client = new Client(1, "Ndong", "Saliou", "saliou@ndong.com");
+
+            Assert.Throws<ArgumentException>(() => client.Nom = "");
+        }
+
+        [Fact]
+        public void TestSetterPrenom_ShouldBeEqual()
+        {
+            Client client = new Client(1, "Ndong", "Saliou", "saliou@ndong.com");
+
+            client.Prenom = "Test";
+
+            Assert.Equal("Test", client.Prenom);
+        }
+
+        [Fact]
+        public void TestSetterNom_ShouldThrowException()
+        {
+            Client client = new Client(1, "Ndong", "Saliou", "saliou@ndong.com");
+
+            Assert.Throws<ArgumentException>(() => client.Prenom = "");
+        }
+    }
+}
---
+++ GestionBanque.Tests/CompteModelTest.cs
@@ -0,0 +1,59 @@
+﻿using GestionBanque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionBanque.Tests
+{
+    public class CompteModelTest
+    {
+        [Fact]
+
+        public void TestDeposer_ShouldBeEqual()
+        {
+            double montantDepot = 45.50;
+            double montantAttendu = 90.05;
+
+            Compte compte = new Compte(1, "cmpt1", 44.55, 2);
+
+            compte.Deposer(montantDepot);
+
+            Assert.Equal(montantAttendu, compte.Balance);
+        }
+
+        [Fact]
+        public void TestDeposer_ShouldThrowException()
+        {
+            double montantDepot = -45.43;
+
+            Compte compte = new Compte(1, "cmpt1", 52.25, 2);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => compte.Deposer(montantDepot));
+        }
+
+        [Fact]
+        public void TestRetirer_ShouldBeEqual()
+        {
+            double montantRetrait = 45.43;
+            double montantAttendu = 60.07;
+
+            Compte compte = new Compte(1, "cmpt1", 105.50, 2);
+
+            compte.Retirer(montantRetrait);
+
+            Assert.Equal(montantAttendu, compte.Balance);
+        }
+
+        [Fact]
+        public void TestRetirer_ShouldThrowException()
+        {
+            double montantRetrait = 45.43;
+
+            Compte compte = new Compte(1, "cmpt1", 0.00, 2);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => compte.Retirer(montantRetrait));
+        }
+    }
+}
